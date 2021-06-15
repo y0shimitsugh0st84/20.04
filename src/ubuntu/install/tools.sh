cf95b426abbdf3dd4536dd0c639137fd1da056ec
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-06-15 06:30:58 +0200

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Install some common tools for further installation"
 apt-get update 
+apt install git-core gperf bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc libc6-dev-i386 x11proto-core-dev libx11-dev libgl1-mesa-dev zip unzip zlib1g-dev -y
 apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
 #apt-get -y install git gcc curl make libxml2-utils flex m4 openjdk-8-jdk lib32stdc++6 libelf-dev libssl-dev python-enum34 python-mako syslinux-utils software-properties-common
