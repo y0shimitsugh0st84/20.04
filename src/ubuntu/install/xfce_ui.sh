9f811ffc43998540bbb89888f437e750a4c5b726
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-27 18:45:51 +0200

    Update xfce_ui.sh

--- src/ubuntu/install/xfce_ui.sh
+++ src/ubuntu/install/xfce_ui.sh
@@ -11,7 +11,7 @@ apt install --fix-missing -y vim wget ca-certificates  pulseaudio xrdp\
   openssh-server sudo git build-essential cmake libuv1-dev uuid-dev libmicrohttpd-dev libssl-dev \
   nano  xvfb \
   python3-pip  locate  libxml2-dev libxslt1-dev libssl-dev libmicrohttpd-dev  \
-  libmysqlclient-dev byobu locate cron python-pyaudio python3-pyaudio ffmpeg \
+  libmysqlclient-dev byobu locate cron  python3-pyaudio ffmpeg \
   fonts-liberation libappindicator3-1 libfile-basedir-perl libfile-desktopentry-perl libfile-mimeinfo-perl \
   libindicator3-7  libipc-system-simple-perl libnet-dbus-perl libtie-ixhash-perl libx11-protocol-perl \
   libxml-parser-perl libxml-twig-perl libxml-xpathengine-perl xdg-utils  xserver-xephyr jq tor xarchiver libavcodec-extra vlc
